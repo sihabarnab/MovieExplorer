02cfc5e7dc7fd32d08722b297828ddbe9dc929dc
Sihab Hossain <sihabarnab123@gmail.com>
2025-02-19 20:42:39 +0600

    Initial commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "71cae132c54d684c00cf794381c6e2e0";
+const BASE_URL = "https://api.themoviedb.org/3"; 
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
